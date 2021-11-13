9ce5da458cd2b2f9a227cd3fb5735650fb59c2db
AzureAD\OfeliaCocucciFurlan <ofelia.cocucci@imotionanalytics.com>
2021-11-13 11:54:29 +0100

    Gets and sorts fruits from psychodelic chaos

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        fruits: [],
+    },
+    actions: {
+        loadFruits({ commit }) {
+            axios
+                .get('http://localhost:3000/fruit')
+                .then(r => r.data)
+                .then(fruits => {
+                    commit('SET_FRUITS', fruits)
+                })
+        }
+    },
+    mutations: {
+        SET_FRUITS(state, fruits) {
+            getFruitsFromChaos(fruits.data, state);
+        }
+    },
+    modules: {}
 })
+
+var getFruitsFromChaos = async(data, state) => {
+    let fruits = traverse(data);
+    fruits.forEach(fruit => {
+        state.fruits.push(fruit);
+    });
+
+    state.fruits.sort((a, b) => a.id - b.id);
+}
+
+function traverse(jsonObj) {
+    let fruits = [];
+    if (jsonObj !== null && typeof jsonObj == "object") {
+        Object.entries(jsonObj).forEach(([key, value]) => {
+            let returnFruits = traverse(value);
+            returnFruits.forEach(fruit => { fruits.push(fruit); });
+            if (key == 'isFruit' && value === true) { fruits.push(jsonObj); }
+        });
+    }
+    return fruits;
+}
