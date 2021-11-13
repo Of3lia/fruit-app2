bd02bb3a1b9860643a4fbe9f3b655f02bca471eb
AzureAD\OfeliaCocucciFurlan <ofelia.cocucci@imotionanalytics.com>
2021-11-13 16:22:33 +0100

    Enhance fruit component

--- src/components/AllFruits.vue
+++ src/components/AllFruits.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <span v-for="(fruit, i) of fruits" v-bind:key="i">
-      <div v-if="fruit.id">{{ fruit }}</div>
+    <v-container>
+      <v-row>
+        <v-col
+          xs="12"
+          sm="6"
+          md="4"
+          lg="4"
+          xl="4"
+          v-for="(fruit, i) in fruits"
+          :key="i"
+        >
+          <Fruit v-if="fruit.id" :fruit="fruit" />
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- <span v-for="(fruit, i) of fruits" v-bind:key="i">
+      <div v-if="fruit.id"><Fruit /></div>
       <br />
-    </span>
-
-    <Fruit />
+    </span> -->
   </div>
 </template>
 
--- src/components/Fruit.vue
+++ src/components/Fruit.vue
@@ -1,10 +1,65 @@
 <template>
-  <div>asdasdñlds ñlkas sd adskñlasdk qwoe iqpwo ke</div>
+  <div>
+    <v-card :loading="loading">
+      <template slot="progress">
+        <v-progress-linear
+          :color="fruit.color"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+
+      <v-img height="250" :src="fruit.image"></v-img>
+
+      <v-card-actions>
+        <v-card-title>{{
+          fruit.name.charAt(0).toUpperCase() + fruit.name.slice(1)
+        }}</v-card-title>
+        <v-spacer></v-spacer>
+        <div>$ 550</div>
+      </v-card-actions>
+
+      <v-card-actions>
+        <v-btn color="orange lighten-2" text> Eat </v-btn>
+        <v-btn color="orange lighten-2" text> Modify </v-btn>
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            {{ fruit.description }}
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Fruit",
+  props: {
+    fruit: Object,
+  },
+  data: () => ({
+    loading: false,
+    // selection: 1,
+    show: false,
+  }),
+  methods: {
+    reserve() {
+      this.loading = true;
+
+      setTimeout(() => (this.loading = false), 2000);
+    },
+  },
 };
 </script>
 
