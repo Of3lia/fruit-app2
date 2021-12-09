a68d925b42caf953d1bd8127050d28ac8ed3a4bc
Of3lia <ofeliacocucci@outlook.com>
2021-12-09 15:13:12 +0100

    test2

--- src/router/index.js
+++ src/router/index.js
@@ -6,28 +6,28 @@ import Fruit from './../components/Fruit.vue'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/fruit/:id',
-        name: 'Fruit',
-        component: Fruit
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '/',
+    name: 'Home',
+    component: Home
+},
+{
+    path: '/fruit/:id',
+    name: 'Fruit',
+    component: Fruit
+},
+{
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import( /* webpackChunkName: "about" */ '../views/About.vue')
+}
 ]
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: 'hash',
     base: process.env.BASE_URL,
     routes
 })
